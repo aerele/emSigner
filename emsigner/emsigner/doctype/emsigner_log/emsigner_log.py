14dd7e7d10a5df193747e225ae0aab2282236fbf
vimalraj27 <vimalraj2772000@gmail.com>
2025-02-13 16:28:00 +0530

    feat: create emsigner logs doctype and clear logs automatically

--- emsigner/emsigner/doctype/emsigner_log/emsigner_log.py
+++ emsigner/emsigner/doctype/emsigner_log/emsigner_log.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2025, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+from datetime import datetime, timedelta
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 
 class emSignerLog(Document):
 	pass
+
+
+def clear_emsigner_logs_after_days_rq_job():
+	frappe.enqueue(
+		clear_emsigner_logs_after_days,
+		queue="long",
+		is_async=True,
+		job_name="emSigner Log cleanup",
+	)
+
+
+def clear_emsigner_logs_after_days():
+	no_of_days = frappe.db.get_value("emSigner Settings", "emSigner Settings", "clear_logs_after_days")
+	delete_upto = (datetime.today() - timedelta(days=cint(no_of_days))).date()
+	frappe.db.delete("emSigner Log", {"creation": ["<", f"{delete_upto} 23:59:59"]})
